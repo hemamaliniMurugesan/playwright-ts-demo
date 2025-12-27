75bd4903c0702965d88c3b0d29bf31cc047e3886
hemamaliniMurugesan <105474448+hemamaliniMurugesan@users.noreply.github.com>
2025-12-27 15:11:01 +0000

    open AI page

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,7 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
-test('Open Google', async ({ page }) => {
-  await page.goto('https://www.google.com');
-  await expect(page).toHaveTitle(/Google/);
+test('open google', async({page})=>
+{
+await page.goto('https://in.skillup.online/artificial-intelligence-courses-and-programs/?utm_source=websiteinternal&utm_medium=megamenu&utm_campaign=NA');
+await expect(page).toHaveTitle('Artificial Intelligence');
 });

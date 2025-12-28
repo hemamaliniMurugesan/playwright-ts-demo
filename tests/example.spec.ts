abefc1dce26a2c5561810028a5e77f6717061867
hemamaliniMurugesan <105474448+hemamaliniMurugesan@users.noreply.github.com>
2025-12-28 15:10:15 +0000

    fullname text field

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,4 +4,5 @@ test('open google', async({page})=>
 {
 await page.goto('https://qa-in.skillup.online/artificial-intelligence-courses-and-programs/?utm_source=websiteinternal&utm_medium=megamenu&utm_campaign=NA');
 await expect(page).toHaveTitle(/Artificial Intelligence/);
+await page.getByPlaceholder('Full Name').fill('hemamalini@gmail.com');
 });

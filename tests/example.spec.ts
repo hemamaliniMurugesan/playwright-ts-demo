5df3b26061ca77d1b28e78c385f6f6246e33724d
hemamaliniMurugesan <105474448+hemamaliniMurugesan@users.noreply.github.com>
2025-12-28 15:41:43 +0000

    check locators

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,9 +4,13 @@ test('open google', async({page})=>
 {
 await page.goto('https://qa-in.skillup.online/artificial-intelligence-courses-and-programs/?utm_source=websiteinternal&utm_medium=megamenu&utm_campaign=NA');
 await expect(page).toHaveTitle(/Artificial Intelligence/);
-await page.getByPlaceholder('Full Name').fill('hemamalini@gmail.com');
+await page.getByPlaceholder('Full Name').fill('hemamalini@skillup.tech');
+await page.getByLabel('Email').fill('Test@123');
 if(page.url().includes('-in.skillup.online'))
 {
     await expect(page.getByText('Placement')).toBeVisible;
 }
+const locateButton = page.getByRole('button',{name : 'Subscribe'});
+await locateButton.hover();
+await locateButton.click();
 });

257beb645ed273142839341d37d0a633259885e0
hemamaliniMurugesan <105474448+hemamaliniMurugesan@users.noreply.github.com>
2025-12-29 06:32:32 +0000

    form validation

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,8 +4,8 @@ test('open google', async({page})=>
 {
 await page.goto('https://qa-in.skillup.online/artificial-intelligence-courses-and-programs/?utm_source=websiteinternal&utm_medium=megamenu&utm_campaign=NA');
 await expect(page).toHaveTitle(/Artificial Intelligence/);
-await page.getByPlaceholder('Full Name').fill('hemamalini@skillup.tech');
-await page.getByLabel('Email').fill('Test@123');
+await page.getByPlaceholder('Full Name').fill('hema malini');
+await page.getByLabel('Email').fill('hemamalini@skillup.tech');
 const locateButton = page.getByRole('button',{name : 'Subscribe'});
 await locateButton.hover();
 await locateButton.click();
